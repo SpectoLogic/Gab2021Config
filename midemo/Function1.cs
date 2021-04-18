e46c92fa9dc02de082cae2115c58dd7c8543275d
Andreas Pollak <apollak@hotmail.com>
2021-04-18 18:47:15 +0200

    Demo-1 (Managed Identity) finished

--- midemo/Function1.cs
+++ midemo/Function1.cs
@@ -1,11 +1,12 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
+using Azure.Identity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace midemo
 {
@@ -17,18 +18,36 @@ namespace midemo
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+                // DefaultAzureCredential mi = new DefaultAzureCredential();
+                Azure.Identity.ManagedIdentityCredential mi = new Azure.Identity.ManagedIdentityCredential();
 
-            string name = req.Query["name"];
+                string container = "demo";
+                
+                Azure.Storage.Blobs.BlobContainerClient containerClient = new Azure.Storage.Blobs.BlobContainerClient(
+                    new Uri($"https://midemo2.blob.core.windows.net/{container}"), 
+                    mi);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+                using Stream streamData = ToStream("Hello World!");
+                await containerClient.UploadBlobAsync("demo.txt", streamData);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.ToString());
+            }
+            return new OkResult();
+        }
 
-            return new OkObjectResult(responseMessage);
+        private static Stream ToStream(string text)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(text);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
         }
     }
 }
